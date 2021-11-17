71c241c82a06727ad7bff46482b3cec1d7add866
Gotsmy <mathias.gotsmy@univie.ac.at>
2021-11-17 13:53:00 +0100

    Moved normalization functions to normalization.py

--- synthetic_data/run_simulation_v2.py
+++ synthetic_data/run_simulation_v2.py
@@ -7,96 +7,9 @@ import pandas as pd
 import sys
 import extended_model as em
 import synthetic_data_generation as sdg
+import normalization as norm
 import pickle
 import time
-
-def calculate_pqn(m_tensor):
-    '''
-    Calculate normalization factor according to PQN as described by Filzmoser et al., 2014.
-    -
-    Input:
-    m_tensor     Numpy array of shape (n_metabolites * n_timepoints) of measured data.
-    -
-    Output:
-    pqn          Numpy array of shape (n_timepoints) with normalization factors.
-    '''
-    
-    # implemented according to http://dx.doi.org/10.1016/j.chroma.2014.08.050
-    ref = np.median(m_tensor,axis=1) 
-    div = np.divide(m_tensor,ref[:,None])
-    pqn = np.median(div,axis=0)
-    return pqn
-
-def calculate_em(m_tensor,lb,ub,timepoints,n_metabolites,n_cpu,n_replicates,loss_name,lambda_):
-    '''
-    Calculate normalization factor according to an EM model.
-    -
-    m_tensor         numpy.ndarray of shape (n_metabolites * n_timepoints) of measured data.
-    lb               numpy.ndarray of shape (n_metabolites * 5 + n_timepoints) of lower bounds of parameters for model fitting.
-    ub               numpy.ndarray of shape (n_metabolites * 5 + n_timepoints) of upper bounds of parameters for model fitting.
-    timepoints       numpy.ndarray of shape (n_timepoints) of time points.
-    n_metabolites    Int. Number of metabolites to fit. 
-    n_cpu            Int. Number of CPUs used in multiprocessing.
-    n_replicates     Int. Number of Monte Carlo replicates used for optimization.
-    loss_name        Str. Loss used for optimization.
-    lambda_          Float. Weighting value for loss calculation.
-    -
-    Output:
-    sweat_volumes    numpy.ndarray of shape (n_timepoints) with normalization factors.
-    model            Optimized em.extended_model.
-    '''
-    
-    # creating model and setting properties
-    model = em.extended_model(timepoints,n_metabolites)
-    model.set_fit_bounds(lb,ub)
-    model.set_measured_data(m_tensor.flatten('F'))
-    model.set_loss_function(loss_name)
-    # the weighting of error residuals is calculated over the sigma array parsed to the underlying scipy.optimize.curve_fit function.
-    sigma_em = 1/lambda_
-    model.set_sigma(np.ones(model.n_timepoints*model.n_metabolites)*sigma_em)
-    # optimization of the model
-    # out is a nd.array of shape ((n_parameters + 1) * n_replicates) with optimized parameters + loss value for every MC replicate.
-    # as we are only interested in the best solution out is not used further.
-    out = model.optimize_monte_carlo(n_replicates=n_replicates,n_cpu=n_cpu)
-    return model.get_sweat_volumes(), model
-
-def calculate_mix(m_tensor,sv_pqn,lb,ub,timepoints,n_metabolites,n_cpu,n_replicates,loss_name,lambda_):
-    '''
-    Calculate normalization factor according to a MIX model.
-    -
-    m_tensor         numpy.ndarray of shape (n_metabolites * n_timepoints) of measured data.
-    sv_pqn           numpy.ndarray of shape (n_timepoints) of normalization factors as calculated from PQN.
-    lb               numpy.ndarray of shape (n_metabolites * 5 + n_timepoints) of lower bounds of parameters for model fitting.
-    ub               numpy.ndarray of shape (n_metabolites * 5 + n_timepoints) of upper bounds of parameters for model fitting.
-    timepoints       numpy.ndarray of shape (n_timepoints) of time points.
-    n_metabolites    Int. Number of metabolites to fit. 
-    n_cpu            Int. Number of CPUs used in multiprocessing.
-    n_replicates     Int. Number of Monte Carlo replicates used for optimization.
-    loss_name        Str. Loss used for optimization.
-    lambda_          Float. Weighting value for loss calculation.
-    -
-    Output:
-    sweat_volumes    numpy.ndarray of shape (n_timepoints) with normalization factors.
-    model            Optimized em.extended_model.
-    '''
-    
-    # creating model and setting properties
-    model = em.extended_mix_model(timepoints,n_metabolites)
-    # as the MIX model has one additional parameter the bounds have to be appended.
-    model.set_fit_bounds(np.concatenate([[.1],lb]),
-                           np.concatenate([[10],ub]))
-    model.set_measured_data(m_tensor.flatten('F'),sv_pqn)
-    model.set_loss_function(loss_name)
-    # the weighting of error residuals for EM and PQN part of the MIX model is calculated over the sigma array parsed to the underlying scipy.optimize.curve_fit function.
-    sigma_em = 1/lambda_
-    sigma_pqn= 1/(1-lambda_)
-    model.set_sigma(np.append(np.ones(model.n_timepoints*model.n_metabolites)*sigma_em,np.ones(model.n_timepoints)*sigma_pqn))
-    # optimization of the model
-    # out is a nd.array of shape ((n_parameters + 1) * n_replicates) with optimized parameters + loss value for every MC replicate.
-    # as we are only interested in the best solution out is not used further.
-    out = model.optimize_monte_carlo(n_replicates=n_replicates,n_cpu=n_cpu)
-    return model.get_sweat_volumes(), model
-
 
 if __name__ == "__main__":
     # define basic toy model parameters
@@ -167,21 +80,21 @@ if __name__ == "__main__":
                 
                 ## NORMALIZE FOR SWEAT VOLUME
                 t1 = time.time()
-                sv_pqn                      = calculate_pqn(m_tensor)
+                sv_pqn                      = norm.calculate_pqn(m_tensor)
                 t2 = time.time()
-                sv_em_full, em_full_model   = calculate_em(m_tensor,
+                sv_em_full, em_full_model   = norm.calculate_em(m_tensor,
                                                            full_lb,full_ub,timepoints,n_metabolites,
                                                            n_cpu,n_mc_replicates,loss_name,lambda_)
                 t3 = time.time()
-                sv_mix_full, mix_full_model = calculate_mix(m_tensor,sv_pqn,
+                sv_mix_full, mix_full_model = norm.calculate_mix(m_tensor,sv_pqn,
                                                             full_lb,full_ub,timepoints,n_metabolites,
                                                             n_cpu,n_mc_replicates,loss_name,lambda_)
                 t4 = time.time()
-                sv_em_mini, em_mini_model   = calculate_em(m_tensor[:4,:],
+                sv_em_mini, em_mini_model   = norm.calculate_em(m_tensor[:4,:],
                                                            mini_lb,mini_ub,timepoints,n_known_metabolites,
                                                            n_cpu,n_mc_replicates,loss_name,lambda_)
                 t5 = time.time()
-                sv_mix_mini, mix_mini_model = calculate_mix(m_tensor[:4,:],sv_pqn,
+                sv_mix_mini, mix_mini_model = norm.calculate_mix(m_tensor[:4,:],sv_pqn,
                                                             mini_lb,mini_ub,timepoints,n_known_metabolites,
                                                             n_cpu,n_mc_replicates,loss_name,lambda_)
                 t6 = time.time()
