edb457ca7bced83a02c568b7f839fdda0c87dc1c
Gotsmy <mathias.gotsmy@univie.ac.at>
2022-06-28 10:30:59 +0200

    Added requirements in setup.py

--- setup.py
+++ setup.py
@@ -22,4 +22,11 @@ setuptools.setup(
     python_requires=">=3.7,<3.8",
     include_package_data=True,
     package_data={'': ['data/*.csv']},
+    install_requires=['importlib-metadata==3.7.3',
+'matplotlib==3.3.4',
+'numpy==1.20.1',
+'pandas==1.3.0',
+'pickleshare==0.7.5',
+'scipy==1.7.0',
+'tqdm==4.50.0',]
 )
